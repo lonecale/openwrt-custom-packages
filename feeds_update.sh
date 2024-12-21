67c9bbbd1c342683df8f992e4f4c73a7898b56ac
lonecale <54177993+lonecale@users.noreply.github.com>
2024-12-22 03:06:38 +0800

    Update feeds_update.sh

--- feeds_update.sh
+++ feeds_update.sh
@@ -1,9 +1,24 @@
 #!/bin/bash -e
 
+svn_export() {
+	# 参数1是分支名, 参数2是子目录, 参数3是目标目录, 参数4仓库地址
+ 	echo -e "clone $4/$2 to $3"
+	TMP_DIR="$(mktemp -d)" || exit 1
+ 	ORI_DIR="$PWD"
+	[ -d "$3" ] || mkdir -p "$3"
+	TGT_DIR="$(cd "$3"; pwd)"
+	git clone --depth 1 -b "$1" "$4" "$TMP_DIR" >/dev/null 2>&1 && \
+	cd "$TMP_DIR/$2" && rm -rf .git >/dev/null 2>&1 && \
+	cp -af . "$TGT_DIR/" && cd "$ORI_DIR"
+	rm -rf "$TMP_DIR"
+}
+# svn_export "master" "applications/luci-app-wechatpush" "feeds/luci/applications/luci-app-wechatpush" "https://github.com/immortalwrt/luci"
+
 # backup feeds
 shopt -s extglob
 mkdir -p /tmp/extd/
 mv */ /tmp/extd/
+
 
 # download
 git clone https://github.com/sirpdboy/sirpdboy-package sirpdboy-package -b main --depth 1
