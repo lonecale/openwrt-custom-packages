d39e9b6b966beb2f19654b694e5cb786258f7ef7
lonecale <54177993+lonecale@users.noreply.github.com>
2025-03-16 00:40:09 +0800

    Update feeds_update.sh

--- feeds_update.sh
+++ feeds_update.sh
@@ -56,9 +56,12 @@ rm -rf luci-app-filemanager/.git
 git clone https://github.com/sbwml/package_libs_ngtcp2 ngtcp2 --depth 1
 rm -rf ngtcp2/.git
 
-git clone https://github.com/sbwml/OpenAppFilter luci-app-oaf --depth 1
-sed -i '/#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 12, 0))/,/#endif/d' luci-app-oaf/oaf/src/af_log.c
-rm -rf luci-app-oaf/{.git,LICENSE}
+# git clone https://github.com/sbwml/OpenAppFilter luci-app-oaf --depth 1
+# sed -i '/#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 12, 0))/,/#endif/d' luci-app-oaf/oaf/src/af_log.c
+# rm -rf luci-app-oaf/{.git,LICENSE}
+
+git clone https://github.com/destan19/OpenAppFilter luci-app-oaf -b master --depth 1
+rm -rf OpenAppFilter/{.git,LICENSE}
 
 # git clone https://github.com/gdy666/luci-app-lucky luci-app-lucky --depth 1
 git clone https://github.com/sirpdboy/luci-app-lucky luci-app-lucky --depth 1
